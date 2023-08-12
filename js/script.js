af4637ccc39d24cac76853edfe2b9740ef1d12e8
dannycastilloo <dannycastillootiniano@gmail.com>
2023-08-12 18:50:18 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+let url_base = 'https://rickandmortyapi.com/api/character'
+
+document.getElementById('searchButton').addEventListener('click', () => {
+      const names = document.getElementById('characterEntry').value
+      if (names) {
+            fetchCharacterData(names)
+      }
+})
+
+function fetchCharacterData(names){
+      fetch(`${url_base}/?name=${names}`)
+            .then(response => response.json())
+            .then(response => showCharacterData(response.results))
+}
+
+function showCharacterData(results) {
+      const characterContainer = document.getElementById('characterContainer')
+      characterContainer.innerHTML = ''
+
+      results.forEach(result => {
+            const characterDiv = document.createElement('div');
+
+            const name = result.name
+            const characterName = document.createElement('h1')
+            characterName.textContent = name
+
+            const image = result.image
+            const characterImage = document.createElement('img')
+            characterImage.src = image
+
+            const status = result.status
+            const characterStatus = document.createElement('h2')
+            characterStatus.textContent = status
+
+            const species = result.species
+            const characterSpecies = document.createElement('h2')
+            characterSpecies.textContent = species
+
+            const gender = result.gender
+            const characterGender = document.createElement('h2')
+            characterGender.textContent = gender
+
+            characterDiv.appendChild(characterName)
+            characterDiv.appendChild(characterImage)
+            characterDiv.appendChild(characterStatus)
+            characterDiv.appendChild(characterSpecies)
+            characterDiv.appendChild(characterGender)
+
+            characterContainer.appendChild(characterDiv)
+      })
+      
+}
